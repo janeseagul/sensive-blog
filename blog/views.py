716de8e2862de29d6afdde7b24a216466540c964
Marta Chaika <janeseagul@gmail.com>
2023-04-18 13:13:27 +0300

    optimize comments count evaluation

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,21 @@ from django.db.models import Count
 
 
 def get_related_posts_count(tag):
-    return tag.tagged_posts
+    return tag.posts.count()
+
+
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': len(Comment.objects.filter(post=post)),
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
 
 
 def serialize_post(post):
@@ -33,19 +47,27 @@ def get_likes_count(post):
 
 
 def index(request):
-    most_popular_posts = Post.objects.annotate(num_likes=Count('likes')).order_by('-num_likes')[:5].prefetch_related(
-        "author")
+    most_popular_posts = Post.objects.annotate(
+        likes_count=Count('likes'),
+    ).prefetch_related('author').order_by('-likes_count')[:5]
+    most_popular_posts_ids = [post.id for post in most_popular_posts]
+    post_with_comments = Post.objects.filter(id__in=most_popular_posts_ids).annotate(
+        comments_count=Count('comments'),
+    )
+    ids_and_comments = post_with_comments.values_list('id', 'comments_count')
+    count_for_id = dict(ids_and_comments)
+
+    for post in most_popular_posts:
+        post.comments_count = count_for_id[post.id]
 
     fresh_posts = Post.objects.order_by('published_at').prefetch_related('author')
-    most_fresh_posts = list(fresh_posts)[-5:]
+    most_fresh_posts = Post.objects.annotate(Count('comments')).prefetch_related('author').order_by('-published_at')[:5]
 
-    most_popular_tags = Tag.objects.annotate(tagged_posts=Count('posts')).order_by('-tagged_posts')[:5]
+    most_popular_tags = Tag.objects.annotate(posts_count=Count('posts'),).order_by('-posts_count')[:5]
 
     context = {
-        'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
-        ],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
+        'most_popular_posts': [serialize_post_optimized(post) for post in most_popular_posts],
+        'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
